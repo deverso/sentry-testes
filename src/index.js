285c89d41a917270f837b6fe9881a880d5437129
Danilo Deverso <deverso@gmail.com>
2019-11-12 18:13:54 -0300

    New release

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import * as Sentry from '@sentry/browser';
 import App from './app'
 
 Sentry.init({
-  release: 'beta-01',
+  release: 'beta-02',
   dsn: "https://56c0254584ef4c329622df7c88b3e672@sentry.io/1816186"
 });
 
